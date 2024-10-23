aa22c10c5b8f4f1acfbad13ec583b97676ef332e
Jahongir <webdevoloper97@gmail.com>
2024-10-23 16:18:29 +0500

    Update class.py

--- class.py
+++ class.py
@@ -9,10 +9,8 @@ class User:
     def get_info(self):
         return f"Foydalanuvchi: {self.username}, ismi: {self.full_name}, email: {self.email}, yosh: {self.age}, joylashuvi: {self.location}"
 
-# Bir nechta foydalanuvchi obyektlarini yaratamiz
 user1 = User("Alijon Valiyev", "alijon1994", "alijon1994@gmail.com", 30, "Toshkent")
 user2 = User("Aliya Mirzaeva", "aliya_m", "aliya_m@gmail.com", 28, "Samarqand")
 
-# Obyektlarning metodlarini chaqirish
 print(user1.get_info())
 print(user2.get_info())
